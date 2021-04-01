2a3db3df8fb926444c636f0d0ce40420edf2bba0
Akovancev <apm120700@gmail.com>
2021-04-01 16:55:28 +0300

    Timer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
     private static ArrayList Alist = new ArrayList();
     private static LinkedList Llist = new LinkedList();
     private static int N = 1000;
+    private static long [][] arr = new long[2][3];
 
     private static void addArray() {
         for (int i = 0; i < N; i++) {
@@ -40,26 +41,34 @@ public class Main {
         }
     }
 
-    public static void timer(Function func) {
+    public static void timer() {
         long startTime = System.currentTimeMillis();
-
+        addArray();
+        long endTime = System.currentTimeMillis();
+        arr[0][0] = endTime-startTime;
+        startTime = System.currentTimeMillis();
+        addLinked();
+        endTime = System.currentTimeMillis();
+        arr[1][0] = endTime-startTime;
+        startTime = System.currentTimeMillis();
+        deleteArray();
+        endTime = System.currentTimeMillis();
+        arr[0][1] = endTime-startTime;
+        startTime = System.currentTimeMillis();
+        deleteLinked();
+        endTime = System.currentTimeMillis();
+        arr[1][1] = endTime-startTime;
+        startTime = System.currentTimeMillis();
+        getArray();
+        endTime = System.currentTimeMillis();
+        arr[0][2] = endTime-startTime;
+        startTime = System.currentTimeMillis();
+        getLinked();
+        endTime = System.currentTimeMillis();
+        arr[1][2] = endTime-startTime;
     }
 
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        Function func = Alist::add;
-        timer(func);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Total execution time: " + (endTime-startTime) + "ms");
-        startTime = System.currentTimeMillis();
-        func = Alist::remove;
-        timer(func);
-        endTime = System.currentTimeMillis();
-        System.out.println("Total execution time: " + (endTime-startTime) + "ms");
-        startTime = System.currentTimeMillis();
-        //func = Llist::get;
-        timer(func);
-        endTime = System.currentTimeMillis();
-        System.out.println("Total execution time: " + (endTime-startTime) + "ms");
+        timer();
     }
 }
