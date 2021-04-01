132ecc3d88a4a2a2100f2b23b8dfb0d8354db0fe
Akovancev <apm120700@gmail.com>
2021-04-01 16:48:59 +0300

    Method to add, delete and get at ArrayList and LinkedList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,42 @@ public class Main {
     private static LinkedList Llist = new LinkedList();
     private static int N = 1000;
 
-    private static void timer(Function<String, ArrayList> func) {
+    private static void addArray() {
         for (int i = 0; i < N; i++) {
-            func.apply(Integer.toString(i));
+            Alist.add(i);
         }
+    }
+    private static void addLinked() {
+        for (int i = 0; i < N; i++) {
+            Llist.add(i);
+        }
+    }
+
+    private static void deleteArray() {
+        for (int i = 0; i < N; i++) {
+            Alist.remove(0);
+        }
+    }
+    private static void deleteLinked() {
+        for (int i = 0; i < N; i++) {
+            Llist.remove(0);
+        }
+    }
+
+    private static void getArray() {
+        for (int i = 0; i < N; i++) {
+            Alist.get(0);
+        }
+    }
+    private static void getLinked() {
+        for (int i = 0; i < N; i++) {
+            Llist.get(0);
+        }
+    }
+
+    public static void timer(Function func) {
+        long startTime = System.currentTimeMillis();
+
     }
 
     public static void main(String[] args) {
@@ -21,6 +53,11 @@ public class Main {
         System.out.println("Total execution time: " + (endTime-startTime) + "ms");
         startTime = System.currentTimeMillis();
         func = Alist::remove;
+        timer(func);
+        endTime = System.currentTimeMillis();
+        System.out.println("Total execution time: " + (endTime-startTime) + "ms");
+        startTime = System.currentTimeMillis();
+        //func = Llist::get;
         timer(func);
         endTime = System.currentTimeMillis();
         System.out.println("Total execution time: " + (endTime-startTime) + "ms");
