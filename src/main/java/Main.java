c95066ef3c1b2ec0a0a6b80e3d6429af6a8e08e5
Akovancev <apm120700@gmail.com>
2021-04-01 17:31:57 +0300

    Java docs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,80 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Класс сравнения эффективности основных операций ArrayList и LinkedList
+ * @author Павел Акованцев
+ */
 public class Main {
+
+    /** Поле списка коллекции ArrayList для сравнения эффективности*/
     private static ArrayList Alist = new ArrayList();
+    /** Поле списка коллекции LinkedList для сравнения эффективности*/
     private static LinkedList Llist = new LinkedList();
-    private static int N = 1000;
+    /** Поле количество выполненных операций*/
+    private static int N = 10000;
+    /** Поле количество проверяемых методов*/
     private static int countMethods = 3;
+    /** Поле массив для хранения результатов*/
     private static long [][] arr = new long[2][countMethods];
 
+    /**
+     * Функция выполняющая N раз метод add коллекции ArrayList
+     */
     private static void addArray() {
         for (int i = 0; i < N; i++) {
             Alist.add(i);
         }
     }
+
+    /**
+     * Функция выполняющая N раз метод add коллекции LinkedList
+     */
     private static void addLinked() {
         for (int i = 0; i < N; i++) {
             Llist.add(i);
         }
     }
 
+    /**
+     * Функция выполняющая N раз метод delete коллекции ArrayList
+     */
     private static void deleteArray() {
         for (int i = 0; i < N; i++) {
-            Alist.remove(0);
+            Alist.remove(i);
         }
     }
+
+    /**
+     * Функция выполняющая N раз метод delete коллекции LinkedList
+     */
     private static void deleteLinked() {
         for (int i = 0; i < N; i++) {
-            Llist.remove(0);
+            Llist.remove(i);
         }
     }
 
+    /**
+     * Функция выполняющая N раз метод get коллекции ArrayList
+     */
     private static void getArray() {
         for (int i = 0; i < N; i++) {
-            Alist.get(0);
-        }
-    }
-    private static void getLinked() {
-        for (int i = 0; i < N; i++) {
-            Llist.get(0);
+            Alist.get(i);
         }
     }
 
+    /**
+     * Функция выполняющая N раз метод get коллекции LinkedList
+     */
+    private static void getLinked() {
+        for (int i = 0; i < N; i++) {
+            Llist.get(i);
+        }
+    }
+
+    /**
+     * Функция записи времени выполнения основных операций коллекций ArrayList и LinkedList в массив arr
+     */
     public static void timer() {
         long startTime = System.currentTimeMillis();
         addArray();
@@ -68,6 +102,9 @@ public class Main {
         arr[1][2] = endTime-startTime;
     }
 
+    /**
+     * Функция вывода полученных данных в консоль
+     */
     public static void print() {
         System.out.print("             add  delete get\n");
         System.out.print("ArrayList:   ");
@@ -81,6 +118,9 @@ public class Main {
         System.out.println("\nCount of operation: " + N);
     }
 
+    /**
+     * Функция main, основная функция программы
+     */
     public static void main(String[] args) {
         addArray();
         addLinked();
